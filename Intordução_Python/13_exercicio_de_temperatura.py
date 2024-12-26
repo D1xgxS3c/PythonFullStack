45db939e48a95de9ec0b68c83adcd6c21074afb4
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-12-26 10:08:36 -0300

    exercicio de temperatura

--- Intordução_Python/13_exercicio_de_temperatura.py
+++ Intordução_Python/13_exercicio_de_temperatura.py
@@ -5,5 +5,4 @@ temperatura_farenheit = float(input('Digite uma temperatura em graus farenheit: 
 
 temperatura_celsius = 5 * ((temperatura_farenheit-32)/9)
 
-
 print(f'A temperatura em graus celsius é de {temperatura_celsius}')
