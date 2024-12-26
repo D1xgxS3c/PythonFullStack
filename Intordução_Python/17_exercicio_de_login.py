778ef109d0cfbd16435e13529740bb3cd205ca25
Dx1gxS3c <diego.cyberdev@gmail.com>
2024-12-26 10:10:38 -0300

    exercicio de login

--- Intordução_Python/17_exercicio_de_login.py
+++ Intordução_Python/17_exercicio_de_login.py
@@ -4,7 +4,6 @@
 usuario = input('Digite o nome do usuário: ')
 senha = int(input('Digite a senha do usuário: '))
 
-
 if usuario == 'Diego' and senha == 1234:
     print('Logado com sucesso!')
 else:
