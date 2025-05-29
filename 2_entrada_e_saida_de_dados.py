5e0ef437b5165de0c14996433d7efc6f0c4b6e97
Dx1gxS3c <diego.cyberdev@gmail.com>
2025-05-29 11:06:20 -0300

    entrada e saida de dados

--- 2_entrada_e_saida_de_dados.py
+++ 2_entrada_e_saida_de_dados.py
@@ -1,8 +1,13 @@
 #Entrada e saída de dados
 
-idade = 21
+'''idade = 21
 nome = "João"
 
 resultado = f"Olá {nome}, você tem {idade} anos."
-print(resultado)
+print(resultado)'''
 
+
+idade = input("Digite sua idade: ")
+nome = input("Digite seu nome: ")
+
+print(f"Olá {nome}, você tem {idade} anos.")
