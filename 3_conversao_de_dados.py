598835bac3fd35cec274e7f6635f3e3fba192f74
Dx1gxS3c <diego.cyberdev@gmail.com>
2025-05-29 11:14:56 -0300

    conversão de dados

---
+++ 3_conversao_de_dados.py
@@ -0,0 +1,8 @@
+#Conversão de dados
+
+idade = "21"
+idade = int(idade)  # Convertendo string para inteiro
+
+tipo = type(idade)
+print(f"Idade convertida: {idade}")
+print(f"Tipo da variável idade: {tipo}")
