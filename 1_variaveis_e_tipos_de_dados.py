f141b09aae29dc16532149fab57e01325ea6f865
Dx1gxS3c <diego.cyberdev@gmail.com>
2025-05-29 10:41:31 -0300

    vairiáveis e tipos de dados

--- 1_variaveis_e_tipos_de_dados.py
+++ 1_variaveis_e_tipos_de_dados.py
@@ -5,6 +5,16 @@
 # -> Processar dados
 # -> Exibir dados
 
+# -> int (inteiro(número inteiro))
+# -> float (ponto flutuante)
+# -> complex (número complexo)
+# -> str (string)
+# -> bool (booleano) True ou False
+
+# Variável do tipo inteiro
+
 idade = 21
+idade = 22
 print(idade)
+print( type(idade))
 
